95207b7020129d7860a80136bb84413c57ba9431
Naim <mdnaim01303202218@gmail.com>
2023-12-07 01:06:04 +0600

    learn about dynamic router

--- practice-backend/index.js
+++ practice-backend/index.js
@@ -3,9 +3,39 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(function(req, res, next){
+    console.log('hello form middleware');
+    next();
+})
+app.use(function(req, res, next){
+    console.log('hello form middleware2');
+    next();
+})
+
 app.get('/' , (req, res)=>{
     res.send('hi my name is naim islam ')
 })
+
+
+//to make any route dynamic we use (:) at the place where you want to make it dynamic and to access the value use req.params 
+
+app.get('/facebook/:username' , (req, res)=>{
+    let username = req.params.username;
+    res.send(`hi this is ${username}'s facebook id name`)
+})
+app.get('/facebook', (req , res)=>{
+    res.send('hi i am facebook ')
+})
+
+app.get('/profile', (req , res)=>{
+    res.send('hi i am profile ')
+})
+
+app.get('/twitter', (req, res)=>{
+    res.send('hi it is twitter page')
+})
+
+
 app.listen(port , (req, res)=>{
     console.log(`server running on ${port}`)
 })
